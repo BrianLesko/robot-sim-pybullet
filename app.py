592740c032d5ca1b66288aebeaeea7bdf7486f8b
Brian Lesko <brianjosephlesko@gmail.com>
2024-01-22 19:12:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,21 +22,30 @@ def setupgui():
 
 setupgui() 
 
+try: 
+    p.disconnect()
+    p.setPhysicsEngineParameter(enableFileCaching=1)
+except:
+    pass 
+
 # Start PyBullet in DIRECT mode
 p.connect(p.DIRECT)
 p.setAdditionalSearchPath(pd.getDataPath())
 p.loadURDF('plane.urdf')
-p.loadURDF('cube_small.urdf', basePosition=[0.0, 0.0, 0.025])
+table_pos = [0,0,-0.625]
+flags = p.URDF_INITIALIZE_SAT_FEATURES
+useFixedBase = True
+xarm = p.loadURDF("xarm/xarm6_robot.urdf", flags = flags, useFixedBase=useFixedBase)
 
 with st.sidebar:
     st.title("Resolution")
-    width = st.slider('width', 128, 1460, 512)
+    width = st.slider('width', 128, 1460, 1080)
     height = width
     aspect = 1
     st.title("Camera Viewing Angle")
-    x = st.slider('x', -1.0, 1.0, 0.44)
-    y = st.slider('y', -1.0, 1.0, -0.32)
-    z = st.slider('z', -1.0, 1.0, 0.33)
+    x = st.slider('x', -1.0, 2.0, .8)
+    y = st.slider('y', -1.0, 2.0, .9)
+    z = st.slider('z', -1.0, 2.0, .6)
     fov = st.slider('fov', 1, 179, 60)
     st.write("Clipping planes do not render stuff outside the near-far range")
     near = st.slider('near', 0.0001, 2.0, 0.001)
@@ -44,30 +53,48 @@ with st.sidebar:
 
 # Camera setup
 view_matrix = p.computeViewMatrix(cameraEyePosition=[x, y, z],
-                                  cameraTargetPosition=[0, 0, 0],
-                                  cameraUpVector=[0, 0, 1])
+                                  cameraTargetPosition=[0, 0, .5],
+                                  cameraUpVector=[0, 0, .5])
 projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
 
-# Capture image with OpenGL renderer
-images = p.getCameraImage(width, height, view_matrix, projection_matrix, renderer=p.ER_BULLET_HARDWARE_OPENGL)
-rgba_buffer_opengl = np.array(images[2], dtype=np.uint8)
-rgba_opengl = np.reshape(rgba_buffer_opengl, (height, width, 4))
+col1, col2, col3 = st.columns([1,6,1])
+with col2: image = st.empty()  
+
+targetValue = 0.5
+while True: 
+
+    jointIndex = 1
+    controlMode = p.POSITION_CONTROL
+    targetValue = targetValue + 0.05
+    p.setJointMotorControl2(bodyUniqueId=xarm, 
+                        jointIndex=jointIndex, 
+                        controlMode=controlMode, 
+                        targetPosition=targetValue)
+    
+    p.stepSimulation()
+
+    # Capture image with OpenGL renderer
+    images = p.getCameraImage(width, height, view_matrix, projection_matrix, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    rgba_buffer_opengl = np.array(images[2], dtype=np.uint8)
+    rgba_opengl = np.reshape(rgba_buffer_opengl, (height, width, 4))
+
+    rgb_opengl = rgba_opengl[:, :, :3]
+
+    with image: st.image(Image.fromarray(rgb_opengl), caption='OpenGL Renderer', use_column_width=True)
 
 # Capture image with Tiny renderer
-images = p.getCameraImage(width, height, view_matrix, projection_matrix, renderer=p.ER_TINY_RENDERER)
-rgba_buffer_tiny = np.array(images[2], dtype=np.uint8)
-rgba_tiny = np.reshape(rgba_buffer_tiny, (height, width, 4))
+# images = p.getCameraImage(width, height, view_matrix, projection_matrix, renderer=p.ER_TINY_RENDERER)
+# rgba_buffer_tiny = np.array(images[2], dtype=np.uint8)
+# rgba_tiny = np.reshape(rgba_buffer_tiny, (height, width, 4))
+# rgb_tiny = rgba_tiny[:, :, :3]
 
 # Disconnect PyBullet
 p.disconnect()
 
 # Convert to RGB and normalize
 rgb_opengl = rgba_opengl[:, :, :3]
-rgb_tiny = rgba_tiny[:, :, :3]
 
 # Create two columns
-col1, col2 = st.columns(2)
-
-# Convert to PIL images and display using Streamlit
-col1.image(Image.fromarray(rgb_opengl), caption='OpenGL Renderer', use_column_width=True)
-col2.image(Image.fromarray(rgb_tiny), caption='Tiny Renderer', use_column_width=True)
+col1, col2, col3 = st.columns([1,5,1])
+col2.image(Image.fromarray(rgb_opengl), caption='OpenGL Renderer', use_column_width=True)
+#col2.image(Image.fromarray(rgb_tiny), caption='Tiny Renderer', use_column_width=True)
